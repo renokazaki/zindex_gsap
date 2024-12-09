395da2c836c60353ddf62da8feed89a7e519be27
renokazaki <uvxmwvhva27ejb67dkcr@withone.co.jp>
2024-12-09 13:34:28 +0900

    ひな形作成。モデルが移動はまだ

--- src/Components/Title.tsx
+++ src/Components/Title.tsx
@@ -1,8 +1,16 @@
 import { Text } from "@react-three/drei";
+import { FC } from "react";
 
-export const Title = ({ position, text }) => {
+// Props の型定義
+interface TitleProps {
+  position: [number, number, number]; // Three.js の座標を表現するタプル型
+  text: string; // 表示するテキスト
+}
+
+// FC（Functional Component）の型を使用
+export const Title: FC<TitleProps> = ({ position, text }) => {
   return (
-    <Text position={position}>
+    <Text position={position} fontSize={1}>
       {text}
       <meshStandardMaterial color="white" />
     </Text>
